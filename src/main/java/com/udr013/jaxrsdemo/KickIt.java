4c4fe83625542b930e2def7382e6e7d3c82d92dd
¨udr013¨ <udr013@gmail.com>
2016-11-02 00:55:35 +0100

    minimal jax-rs app

---
+++ src/main/java/com/udr013/jaxrsdemo/KickIt.java
@@ -0,0 +1,10 @@
+package com.udr013.jaxrsdemo;
+
+// this is the application class for jaxrs
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+//this annotation looks for all resources with @Path and expose them on this path
+@ApplicationPath("webapi")
+public class KickIt extends Application {
+}
---
+++ src/main/java/com/udr013/jaxrsdemo/Resource.java
@@ -0,0 +1,16 @@
+package com.udr013.jaxrsdemo;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/blabla")
+public class Resource {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String testMethod(){
+		return " here we go!";
+	}
+}
